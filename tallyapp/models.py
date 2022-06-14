86016ce37fb0e7cd9abf2b826a5adfea59179f38
SruthiVenugopal <sruthivenugopal96@gmail.com>
2022-06-14 17:01:05 +0530

    third commit

--- tallyapp/models.py
+++ tallyapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# class contra(models.Model):
+#     particulars=models.ForeignKey(ledgers,on_delete=models.CASCADE,null=True,blank=True)
+#     credit=models.IntegerField()
+#     debit=models.IntegerField()
