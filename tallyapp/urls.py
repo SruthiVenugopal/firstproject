74c9c2edc095f62ea431c7c2d04da252dd32f36c
SruthiVenugopal <sruthivenugopal96@gmail.com>
2022-06-15 16:45:58 +0530

    fifth commit

--- tallyapp/urls.py
+++ tallyapp/urls.py
@@ -2,5 +2,10 @@ from django.urls import path,include
 from . import views
 urlpatterns=[
     path('',views.index,name='index'),
-    path('page1',views.page1,name='page1')
+    path('page1',views.page1,name='page1'),
+    path('payment',views.payment,name='payment'),
+    path('receipt',views.receipt,name='receipt'),
+    path('journal',views.journal,name='journal'),
+    path('daybook',views.daybook,name='daybook')
 ]
+   
--- tallyapp/views.py
+++ tallyapp/views.py
@@ -5,3 +5,15 @@ def index(request):
     return render(request,'index.html')
 def page1(request):
     return render(request,'page1.html')
+def payment(request):
+    return render(request,'payment.html')
+def receipt(request):
+    return render(request,'receipt.html')
+def journal(request):
+    return render(request,'journal.html')
+# def sales(request):
+#     return render(request,'sales.html')
+# def purchase(request):
+#     return render(request,'purchase.html')
+def daybook(request):
+    return render(request,'daybook.html')
