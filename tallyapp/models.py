3e8a8032ee4941087e1bac2eae7d3d82d153c92c
SruthiVenugopal <sruthivenugopal96@gmail.com>
2022-06-17 16:50:47 +0530

    seventh commit

--- tallyapp/models.py
+++ tallyapp/models.py
@@ -1,7 +1,44 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
-# class contra(models.Model):
-#     particulars=models.ForeignKey(ledgers,on_delete=models.CASCADE,null=True,blank=True)
-#     credit=models.IntegerField()
-#     debit=models.IntegerField()
+
+
+class groups(models.Model):
+    group=models.CharField(max_length=225)
+
+
+    def _str_(self):
+     return self.group
+
+class ledger(models.Model):
+    group=models.ForeignKey(groups,on_delete=models.CASCADE,blank=False)
+    name=models.CharField(max_length=225)
+
+    def _str_(self):
+     return self.name
+
+class transactiontype(models.Model):
+    transactiontype=models.CharField(max_length=225)
+
+
+class contra(models.Model):
+    no=models.IntegerField()
+    account=models.CharField()
+    Particulars=models.ForeignKey(ledger,on_delete=models.CASCADE,blank=False)
+    amount=models.IntegerField()
+    transactiontype=models.ForeignKey(transactiontype,on_delete=models.CASCADE,blank=False)
+    def __str__(self):
+     return self. account
+
+class payment(models.Model):
+    no=models.IntegerField()
+    account=models.CharField()
+    Particulars=models.ForeignKey(ledger,on_delete=models.CASCADE,blank=False)
+    amount=models.IntegerField()
+    transactiontype=models.ForeignKey(transactiontype,on_delete=models.CASCADE,blank=False)
+    def __str__(self):
+     return self. account
+
+
+
