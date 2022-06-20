5619c7df18b12227a5564751c5452ed9d64c1877
SruthiVenugopal <sruthivenugopal96@gmail.com>
2022-06-20 16:45:08 +0530

    nineth commit

--- tallyapp/models.py
+++ tallyapp/models.py
@@ -20,20 +20,9 @@ class ledger(models.Model):
 
 class contra(models.Model):
     date=models.DateField(auto_now=True)
-    no=models.IntegerField()
-    accountchoice= [
-        ('Cash', 'Cash'),
-        ('Hdfc bank', 'Hdfc Bank'),
-        ('Federal bank', 'Federal bank'),
-        ('Sbi bank', 'Sbi bank'),
-        ('Axis bank', 'Axis bank'),
-        ('Bank of baroda', 'Bank if baroda'),
-        ('Union bank', 'Union bank'),
-        ('Uco bank', 'Uco bank')
-        ]
-
-    account=models.CharField(max_length=30,choices=accountchoice,default='cash')
-    Particulars=models.ForeignKey(ledger,on_delete=models.CASCADE,blank=False)
+    no=models.PositiveIntegerField()
+    account=models.ForeignKey(ledger,on_delete=models.CASCADE,blank=False,related_name='account')
+    particulars=models.ForeignKey(ledger,on_delete=models.CASCADE,blank=False,related_name='particulars')
     amount=models.IntegerField()
     def __str__(self):
      return self. account
--- tallyapp/views.py
+++ tallyapp/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render,redirect
+from .models import groups,ledger,contra
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 def page1(request):
-    return render(request,'page1.html')
+    ledgers=ledger.objects.all()
+    context1={'ledgers':ledgers}
+    accledger=legder.objects.filter(group='bank accounts')
+    context2={'accledger':accledger}
+    if request.method=='POST':
+        date=request.POST['date']
+        no=request.POST['no']
+        a=request.POST['account']
+        account1=ledger.objects.get(id=a)
+        b=request.POST['particulars']
+        particular1=legder.objects.get(id=b)
+        amount = request.POST['amount']
+        con=contra(date=date,
+                    no=no,
+                    account=account1,
+                    particulars=particular1,
+                    amount=amount)
+        con.save()
+        print("hii")
+        return redirect('/')
+    return render(request,'page1.html',context1,context2)
 def payment(request):
     return render(request,'payment.html')
 def receipt(request):
